ef9cf06ca1887132c760fbb1998a52e56c82863d
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2014-12-15 02:21:20 -0500

    Update order

--- blocking2.py
+++ blocking2.py
@@ -703,12 +703,12 @@ def naive_blocking(cx, locations, block_row_size, block_col_size, filename):
 #files = [('test_matrices/Tina_AskCal.mtx','test_matrices/Tina_AskCal'),]
 files = [
 	('test_matrices/Tina_AskCal.mtx','test_matrices/Tina_AskCal'),
-	('test_matrices/ch7-9-b5.mtx','test_matrices/ch7-9-b5'),
-	('test_matrices/IG5-18.mtx','test_matrices/IG5-18'),
-	('test_matrices/lp_pds_10.mtx','test_matrices/lp_pds_10'),
-	('test_matrices/modified-webbase-1M.mtx','test_matrices/modified-webbase-1M'),
-	('test_matrices/nh2010.mtx','test_matrices/nh2010'),
 	('test_matrices/rail516.mtx','test_matrices/rail516'),
+	('test_matrices/lp_pds_10.mtx','test_matrices/lp_pds_10'),
+	('test_matrices/ch7-9-b5.mtx','test_matrices/ch7-9-b5'),
+	('test_matrices/IG5-18.mtx','test_matrices/IG5-18'),	
+	('test_matrices/modified-webbase-1M.mtx','test_matrices/modified-webbase-1M'),
+	('test_matrices/nh2010.mtx','test_matrices/nh2010'),	
 	('test_matrices/TF17.mtx','test_matrices/TF17'),
 	('test_matrices/wy2010.mtx','test_matrices/wy2010'),
 	('test_matrices/shar_te2-b3.mtx', 'test_matrices/shar_te2-b3'),
